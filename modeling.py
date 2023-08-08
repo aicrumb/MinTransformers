2a8ae6356ab16349e6dfc3e380f0e6b72bd9580d
crumb <aicrumbmail@gmail.com>
2023-08-07 19:28:07 -0500

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -3,38 +3,6 @@ from torch.nn import TransformerDecoderLayer, TransformerDecoder
 from torch import nn
 import torch
 
-"""
-
-I wanted a little control so that I can focus on data filtering alone, no model differences besides size.
-
-Example usage:
-```python
-from modeling import EncoderDecoder, Config
-model = EncoderDecoder(
-    Config(
-        d_model = 1024,
-        d_out = 256,
-        num_layers = 28, # *2
-        num_heads = 64,
-        num_embeddings = 256,
-        position_embeddings = 512,
-        tie_embeddings = True
-    )
-)
-print("init with", sum([p.numel() for p in model.parameters()]), "params")
-# init with 825098752 params
-
-model(
-    torch.randint(0,255,(4,128)),
-    torch.randint(0,255,(4,1))
-).shape
-torch.Size([4, 1, 256])
-```
-
-There is no tokenizer code provided so using one already built from HuggingFace is probably a good option.
-
-"""
-
 class Config:
 	def __init__(self, **kwargs):
 		self.__dict__.update(kwargs)
